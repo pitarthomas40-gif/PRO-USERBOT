55d0f6af3827ae28e1f438519f8093ac4ba5a9f6
pitarthomas40-gif <pitarthomas40@gmail.com>
2026-02-17 16:43:34 +0530

    Update __main__.py

--- Zaid/__main__.py
+++ Zaid/__main__.py
@@ -7,14 +7,14 @@ from Zaid import clients, app, ids
 from flask import Flask
 import threading
 
-app = Flask(__name__)
+web_app = Flask(__name__)
 
-@app.route("/")
+@web_app.route("/")
 def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host="0.0.0.0", port=10000)
+    web_app.run(host="0.0.0.0", port=8080)
 
 threading.Thread(target=run).start()
 
