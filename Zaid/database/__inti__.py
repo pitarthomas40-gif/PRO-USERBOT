deff68326163ac35a6a6ee959ff357e80b52c499
pitarthomas40-gif <pitarthomas40@gmail.com>
2026-02-17 18:38:08 +0530

    Update __inti__.py

--- Zaid/database/__inti__.py
+++ Zaid/database/__inti__.py
@@ -1,13 +1,1 @@
-import os
-from motor.motor_asyncio import AsyncIOMotorClient
-
-# रेंडर पर मौजूद MONGO_DB_URI को कनेक्ट करना
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI")
-
-if MONGO_DB_URI:
-    cli = AsyncIOMotorClient(MONGO_DB_URI)
-    dbb = cli # यही वो 'dbb' है जिसे gbandb.py ढूँढ रहा है
-else:
-    cli = None
-    dbb = None
-    
+from .dbb import *
