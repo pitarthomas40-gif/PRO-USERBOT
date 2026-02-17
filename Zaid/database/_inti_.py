6e10154122fcefc68bb958de11f504cb1ee8668f
pitarthomas40-gif <pitarthomas40@gmail.com>
2026-02-17 17:35:10 +0530

    Update _inti_.py

--- Zaid/database/_inti_.py
+++ Zaid/database/_inti_.py
@@ -1,6 +1,10 @@
-import motor.motor_asyncio
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
 
-from config import MONGO_URL
-cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+# रेंडर पर आपने जो नाम रखा है (MONGO_DB_URI या MONGO_URL) वही यहाँ लिखें
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI")
 
-dbb = cli.program
+if MONGO_DB_URI:
+    cli = AsyncIOMotorClient(MONGO_DB_URI)
+else:
+    cli = None # अगर URL नहीं मिली तो बॉट क्रैश नहीं होगा
